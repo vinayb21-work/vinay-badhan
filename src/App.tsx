35f976607d8d81de31bb3eda92b70c319b79214e
vinay.badhan <vinay.badhan@medable.com>
2025-12-01 07:02:44 +0530

    Fix React Router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/vinay-badhan">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
